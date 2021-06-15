945d4a74669e381c6a7671d3a5f8085dcc489466
dapplion <35266934+dapplion@users.noreply.github.com>
2021-06-15 11:39:04 +0200

    Bootstrap CRA

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 
-import "./styles.scss";
-
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
